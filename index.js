61e4ed2a1ec5d079ed8cd1df84383002c6ac21dc
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-05-03 00:27:09 +0600

    make api url for all data

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 
-const PORT = process.env.PORT || 5000;
+const chefRecipeData = require("./data/chefRecipeData.json");
+
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-// 
+// home page
+app.get("/", (req, res) => {
+    res.send("welcome to chef recipe server");
+});
 
+
+
+app.listen(port, () => {
+    console.log(`chef recipe listening on port ${port}`);
+});
