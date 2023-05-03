fe43258959245d2e657c76969040f262cb234a39
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-05-03 13:46:31 +0600

    books data created for recipe books shop section

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const app = express();
 
 const chefRecipeData = require("./data/chefRecipeData.json");
-
+const recipeTutorialsData = require("./data/recipeTutorials.json");
+const recipeBooksData = require("./data/booksData.json");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -23,6 +24,16 @@ app.get("/chef-recipe/:id", (req, res) => {
     const id = req.params.id;
     const recipe = chefRecipeData.find(e => e.id === parseInt(id));
     res.send(recipe);
+});
+
+// data for recipe video tutorial
+app.get("/recipe-tutorials", (req, res) => {
+    res.send(recipeTutorialsData);
+});
+
+// data for recipe book shop
+app.get("/recipe-books", (req, res) => {
+    res.send(recipeBooksData);
 });
 
 app.listen(port, () => {
